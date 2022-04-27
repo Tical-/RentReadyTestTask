ce6ac690a30b72a41285a41fdfdeb98fb9084abf
s.seidakhmetov <s.seidakhmetov@temabit.com>
2022-04-27 23:51:26 +0600

    temp2

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using DataverseFunc1;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Xrm.Sdk;
@@ -23,6 +24,8 @@ namespace Tests
         {
             var Start = new DateTime(2022, 02, 11);
             var End = new DateTime(2022, 02, 13);
+            Guid first = Guid.Parse("de3ab269-8517-4785-8cd4-f612ff363b10");
+            Guid second = Guid.Parse("78d61aa4-3c5a-4a93-9adb-4d11d65150d7");
             var request = new DefaultHttpRequest(new DefaultHttpContext())
             {
                 Body = GenerateStreamFromString(GenerateJson(Start, End))
@@ -36,10 +39,23 @@ namespace Tests
             var coll = new EntityCollection(list); 
             var exp = MSDYNAddRange.GetQuery(Start, End);
             mock.Setup(z => z.RetrieveMultipleAsync(exp)).ReturnsAsync(coll);
-            mock.Setup(z => z.CreateAsync(It.IsAny<Entity>())).ReturnsAsync(Guid.NewGuid);
+            //mock.Setup(z => z.CreateAsync(It.IsAny<Entity>())).ReturnsAsync(Guid.NewGuid);
+            var account1 = new Entity("msdyn_timeentry");
+            var test = Start.AddDays(1).ToString("MM.dd.yyyy");
+            account1["msdyn_start"] = Start.AddDays(1).ToString("MM.dd.yyyy");
+            account1["msdyn_end"] = Start.AddDays(1).ToString("MM.dd.yyyy");
+            mock.Setup(z => z.CreateAsync(account1)).ReturnsAsync(first);
+            var account2 = new Entity("msdyn_timeentry");
+            account2["msdyn_start"] = Start.AddDays(2).ToString("MM.dd.yyyy");
+            account2["msdyn_end"] = Start.AddDays(2).ToString("MM.dd.yyyy");
+            mock.Setup(z => z.CreateAsync(account2)).ReturnsAsync(second);
             MSDYNAddRange.svc = mock.Object;
             MSDYNAddRange.exp = exp;
-            await MSDYNAddRange.Run(request, logger);
+            var res = (OkObjectResult)await MSDYNAddRange.Run(request, logger);
+            Assert.NotEqual(((List<Guid>)res.Value)[0], new Guid());
+            Assert.NotEqual(((List<Guid>)res.Value)[1], new Guid());
+            Assert.Equal(((List<Guid>)res.Value)[0], first);
+            Assert.Equal(((List<Guid>)res.Value)[1], second);
         }
     }
 }
